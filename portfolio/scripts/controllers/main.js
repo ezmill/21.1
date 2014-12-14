ae18a6d627698296c2998663d8839d15a4c6e7c1
Ezra Miller <emiller@lsoc.org>
2014-12-13 19:05:02 -0600

    portfolio update

--- portfolio/scripts/controllers/main.js
+++ portfolio/scripts/controllers/main.js
@@ -1,20 +1,52 @@
-'use strict';
-
-/**
- * @ngdoc function
- * @name portfolio2App.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the portfolio2App
- */
-angular.module('portfolio2App')
-  .controller('MainCtrl', function ($scope) {
-
-  });
-
-function angleHandle(){
-
+function bgSetter(string){
+  var bg = document.getElementsByClassName("main-content");
+  // console.log(bg);
+  bg[0].style.background = "url('images/" + string +"')";
 }
 
-function bgSetter(name){
-}
+// document.onkeyup = function(e){
+//   if(e.keyCode == 37){
+//     if($('.left a').length > 0){
+//       window.location = $('.left a')['0'].href;
+//     } else{
+//       window.location = 'http://ezra.io/portfolio/#/ezra_io'
+//     }
+//   }
+
+//   if(e.keyCode == 39){
+//     if($('.right a').length > 0){
+//       window.location = $('.right a')['0'].href;
+//     } else{
+//       window.location = 'http://ezra.io/portfolio/#/syrup'
+//     }
+//   }
+// }
+
+
+
+$(function() {
+    $('.navs').okhover({
+    	el: ".main-content"
+    	// zIndex: 1000
+    });
+
+    $('.navs').click(function(e){
+    	e.preventDefault();
+    	// var ifr = document.createElement("iframe");
+    	// ifr.src = this.href;
+    	// ifr.style.position	= "absolute";
+    	// ifr.style.top = 0;
+    	// ifr.style.left = 0;
+    	// ifr.style.width = "100vw";
+    	// ifr.style.height = "100vh";
+    	// // ifr.style["z-index"] = "10000"; 
+    	// $("body").append(ifr);
+
+        window.open(this.href);
+
+    });
+
+    // $(".top-bar a").click(function(){
+    // 	$("body").find("iframe").remove();
+    // });
+}); 
